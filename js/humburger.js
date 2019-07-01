10b3bf1e147994efc4d3de7b3304ee5f4040fe6c
Irina <irinanchv@yandex.ru>
2019-07-01 21:12:12 +0300

    Start work with Humburger class

---
+++ js/humburger.js
@@ -0,0 +1,29 @@
+class Humburger{
+    constructor(size, stuffing)
+    {
+        this.size = size;
+        this.stuffing = stuffing;
+    }
+
+    toppings = []
+
+    addTopping(topping)
+    {
+        this.toppings.push(topping);
+    }
+
+    removeTopping(topping)
+    {
+
+    }
+
+    getPrice()
+    {
+
+    }
+
+    getCalories()
+    {
+
+    }
+}
